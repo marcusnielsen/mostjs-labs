33666656566c667ae9dc417f6c25ce9fa451cc00
Marcus Nielsen <marcus.nielsen82@gmail.com>
2017-10-24 00:11:13 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { propagateEventTask, runEffects, tap } from "@most/core";
+import { currentTime, newDefaultScheduler } from "@most/scheduler";
+import { create, event } from "most-subject";
+
+// Create a new `Scheduler` for use in our application.
+// Usually, you will want to only have one Scheduler, and it should be shared
+// across your application
+const scheduler = newDefaultScheduler();
+
+// Create our sink and our stream.
+// NOTE: stream is the resulting value of tap(console.log, stream).
+const [sink, stream] = create(tap<number>(console.log));
+
+// Pushes events into our stream.
+const next = (n: number) => event(currentTime(scheduler), n, sink);
+
+// Activate our stream.
+runEffects(stream, scheduler);
+
+// Simulate asynchronous data fetching,
+// and then push values into our stream.
+Promise.resolve([1, 2, 3]).then(data => data.forEach(next));
+
+let i = 0;
+const intervalHandle = setInterval(() => {
+  next(i + 10);
+  i++;
+  if (i >= 10) {
+    clearInterval(intervalHandle);
+  }
+}, 500);
