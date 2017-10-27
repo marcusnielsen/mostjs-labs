f933040c6ad4de411b2fa2abe13f4c9acd9df7ac
Marcus Nielsen <marcus.nielsen82@gmail.com>
2017-10-27 20:37:28 +0200

    maybe and scan

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,42 @@
-import { propagateEventTask, runEffects, tap } from '@most/core'
+import {
+  filter,
+  map,
+  propagateEventTask,
+  runEffects,
+  scan,
+  tap,
+} from '@most/core'
 import { currentTime, newDefaultScheduler } from '@most/scheduler'
-import { ap, compose, fromJust, isJust, Maybe, toMaybe } from '@typed/prelude'
+import { pipe } from '@typed/functions'
+import { ap, fromJust, isJust, Maybe, toMaybe } from '@typed/maybe'
 import { create, event } from 'most-subject'
 
-const initialState = {
+const initialState: any = {
   value: '',
 }
 
 // tslint:disable-next-line:no-console
-const log = (maybe: any) => ap(toMaybe(console.log), maybe)
+const log = console.log
 
 const scheduler = newDefaultScheduler()
 
-const [sink, stream] = create()
+const [sink, setValueStream] = create()
 
 const setValue = (value: Maybe<string>) =>
   event(currentTime(scheduler), value, sink)
 
-runEffects(tap(log, stream), scheduler)
+const resultStream = pipe(
+  filter(isJust),
+  map(fromJust),
+  // @TODO: figure out why the initialState is output last
+  scan((acc, value) => ({ ...acc, value }), initialState),
+  tap(log)
+)(setValueStream)
+
+runEffects(resultStream, scheduler)
 
 setValue(toMaybe('h'))
+setValue(toMaybe(null))
 setValue(toMaybe('he'))
 setValue(toMaybe(null))
 setValue(toMaybe('hej'))
